05cf72bfd786b6c1bf19b6460ab760195f10d1dd
deco0926 <adadennis0926@gmail.com>
2025-04-11 15:57:35 +0800

    change heart

--- src/components/hud/HeartCount.tsx
+++ src/components/hud/HeartCount.tsx
@@ -1,13 +1,15 @@
 import styles from "./HeartCount.module.css";
 import Sprite from "../object-graphics/Sprite";
 import { TILES } from "../../helpers/tiles";
-import PixelNumber from "../object-graphics/PixelNumber";
 
 export default function HeartCount({ level }) {
+  const hearts = Array(level.hpRemaining).fill(null);
+
   return (
     <div className={styles.heartCount}>
-      <Sprite frameCoord={TILES.HEART} />
-      <PixelNumber number={level.hpRemaining} />
+      {hearts.map((_, index) => (
+        <Sprite key={index} frameCoord={TILES.HEART} />
+      ))}
     </div>
   );
 }
